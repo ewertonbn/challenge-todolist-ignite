5ef0a1fc5953edd813fb2f3e8d87e6c68ac2f377
ewertonbn <ewertonbn.dev@gmail.com>
2021-03-13 20:20:25 -0300

    :sparkles: Change path of the logo in the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,8 @@ export function Header() {
   return (
     <header className="header">
       <div>
-        <img src="../logo.svg" alt="to.do"/>
+        {/* <img src="/logo.svg" alt="to.do"/> */}
+        <h1>to.<b>do</b></h1>
       </div>
     </header>
   )
